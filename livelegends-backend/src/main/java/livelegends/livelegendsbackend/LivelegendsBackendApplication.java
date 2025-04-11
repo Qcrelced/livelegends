f4bf7211c28812db34ee9d4e9ecece3b007559b5
Qcrelced <declercq1103@gmail.com>
2025-04-11 15:54:41 +0200

    init project

---
+++ livelegends-backend/src/main/java/livelegends/livelegendsbackend/LivelegendsBackendApplication.java
@@ -0,0 +1,13 @@
+package livelegends.livelegendsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivelegendsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivelegendsBackendApplication.class, args);
+	}
+
+}
---
+++ livelegends-backend/src/test/java/livelegends/livelegendsbackend/LivelegendsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package livelegends.livelegendsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivelegendsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
