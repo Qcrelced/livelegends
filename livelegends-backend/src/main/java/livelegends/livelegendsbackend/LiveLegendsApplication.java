573b2bc356f71d2769c4bf816b2fb964bcd7b562
Qcrelced <declercq1103@gmail.com>
2025-04-19 21:35:21 +0200

    add spring profiles configuration

--- livelegends-backend/src/main/java/livelegends/livelegendsbackend/LiveLegendsApplication.java
+++ livelegends-backend/src/main/java/livelegends/livelegendsbackend/LiveLegendsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LivelegendsBackendApplication {
+public class LiveLegendsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LivelegendsBackendApplication.class, args);
+		SpringApplication.run(LiveLegendsApplication.class, args);
 	}
 
 }
--- livelegends-backend/src/test/java/livelegends/livelegendsbackend/LiveLegendsApplicationTests.java
+++ livelegends-backend/src/test/java/livelegends/livelegendsbackend/LiveLegendsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LivelegendsBackendApplicationTests {
+class LiveLegendsApplicationTests {
 
 	@Test
 	void contextLoads() {
