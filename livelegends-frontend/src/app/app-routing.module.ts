0118ff53279d1ed11c082c211dc0ff7201fb2196
malanant <matis.lavenant@gmail.com>
2025-05-31 22:58:03 +0200

    Add footer and Navbar

--- livelegends-frontend/src/app/app-routing.module.ts
+++ livelegends-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'accueil', component: AccueilComponent},
+  {path: 'footer', component: FooterComponent},
+  {path: 'navbar', component: NavbarComponent},
+
+
+  {
+    path: '',
+    redirectTo: '/accueil',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- livelegends-frontend/src/app/app.module.ts
+++ livelegends-frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccueilComponent
+    AccueilComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
