6b96c4512891164e12ffce3cc1975b6351eb534a
Qcrelced <declercq1103@gmail.com>
2025-06-02 16:58:44 +0200

    add CORS

--- livelegends-backend/src/main/java/livelegends/livelegendsbackend/core/configuration/security/ApplicationSecurityConfig.java
+++ livelegends-backend/src/main/java/livelegends/livelegendsbackend/core/configuration/security/ApplicationSecurityConfig.java
@@ -3,14 +3,19 @@ package livelegends.livelegendsbackend.core.configuration.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -20,9 +25,24 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class ApplicationSecurityConfig {
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:4200")); // autorise Angular
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(false); // si tu utilises les cookies ou sessions
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // applique à toutes les routes
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
--- livelegends-backend/src/main/java/livelegends/livelegendsbackend/webapi/match/MatchController.java
+++ livelegends-backend/src/main/java/livelegends/livelegendsbackend/webapi/match/MatchController.java
@@ -1,9 +1,5 @@
 package livelegends.livelegendsbackend.webapi.match;
 
-import livelegends.livelegendsbackend.core.match.Match;
-import livelegends.livelegendsbackend.core.match.MatchService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,27 +8,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping( "/matchs")
 public class MatchController {
 
-    private final MatchService matchService;
-
-    public MatchController(MatchService matchService) {
-        this.matchService = matchService;
-    }
-
-//    @GetMapping("/matchs")
-//    public List<MatchDto> getMatchs() {
-//        List<MatchDto> matchs = matchService.getAllMatchs();
-//        return matchs;
+//    private final MatchWebApiService matchWebApiService;
+//    private final MatchRepository matchRepository;
+//
+//    public MatchController(MatchWebApiService matchWebApiService, MatchRepository matchRepository) {
+//        this.matchWebApiService = matchWebApiService;
+//        this.matchRepository = matchRepository;
 //    }
 //
-//    @GetMapping("/matchs/{id}")
-//    public MatchDto getMatchsbyId(@PathVariable Long id) {
-//        MatchDto match = matchService.getMatchById(id);
-//        return match;
+//    @GetMapping("/matchs")
+//    public List<Match> getMatchs() {
+//        List<Match> matchs = matchRepository.findAll();
+//        return matchs;
 //    }
 
-    @PostMapping("/matchs")
-    public void createMatch(@RequestBody Match match) {
-
+//    @GetMapping("/matchs/{id}")
+//    public MatchDto getMatchsbyId(@PathVariable Long id) {
+//        MatchDto match = matchWebApiService.getMatchById(id);
+//        return match;
     }
 
-}
+//    @PostMapping("/matchs")
+//    public void createMatch(@RequestBody Match match) {
+//
+//    }
+
+
