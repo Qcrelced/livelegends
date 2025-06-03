2980863ae19891337d506c05c3ff165b26c7ee2c
Qcrelced <declercq1103@gmail.com>
2025-06-03 14:47:04 +0200

    fix web

--- livelegends-backend/src/main/resources/db/dev/data-dev.sql
+++ livelegends-backend/src/main/resources/db/dev/data-dev.sql
@@ -1,9 +1,9 @@
 USE livelegends_dev;
 
-INSERT IGNORE INTO admins (id, email, role)
-VALUES (1, 'test@livelegends.fr', 'ADMIN'),
-       (2, 'support@livelegends.fr', 'SUPPORT'),
-       (3, 'user@livelegends.fr', 'USER')
+INSERT IGNORE INTO admins (id, email, password, role)
+VALUES (1, 'test@livelegends.fr', 'root', 'ADMIN'),
+       (2, 'support@livelegends.fr', 'root','SUPPORT'),
+       (3, 'user@livelegends.fr', 'root','USER')
 ;
 
 
