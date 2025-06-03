53fbe2233eee6ad8aef9e07b12d58258b8c38e7e
Qcrelced <declercq1103@gmail.com>
2025-06-03 13:16:52 +0200

    add profyfills

--- livelegends-backend/src/main/java/livelegends/livelegendsbackend/webapi/player/PlayerController.java
+++ livelegends-backend/src/main/java/livelegends/livelegendsbackend/webapi/player/PlayerController.java
@@ -1,7 +1,8 @@
 package livelegends.livelegendsbackend.webapi.player;
 
-import livelegends.livelegendsbackend.core.player.Player;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
